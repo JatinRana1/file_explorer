c6fa5c439822a31d856c52c65099accd2015c7ee
Jatin Rana <jatinrana120@gmail.com>
2025-06-21 17:39:23 +0530

    auto completer

--- core/UI.js
+++ core/UI.js
@@ -1,29 +1,52 @@
 const readline = require('readline')
-
+const fs = require('fs')
 class UI {
-    
+
     constructor() {
         let input = process.stdin
         let output = process.stdout
 
+        const completer = (line) => {
+            const currentDir = process.cwd();
+
+            const [command, ...rest] = line.trim().split(" ")
+
+            const partial = rest.join(" ")
+            
+            const fileList = fs.readdirSync(currentDir)
+            
+            const results = fileList.filter(file => file.startsWith(partial))
+
+            results.forEach((result, index, array)=> {
+                array[index] = command + ' ' + result
+            })
+
+            return [results.length ? results : fileList, line]
+        };
+
         this.rl = readline.createInterface({
-            input, 
+            input,
             output,
-            prompt: `\n~> ${process.cwd()} > `
+            prompt: `\n~> ${process.cwd()} > `,
+            completer: completer
         })
-        
+
+        this.rl.prompt()
+
+        this.rl.on('close', () => {
+            console.log('\n👋 Exiting File Explorer. Goodbye!')
+        })
+    }
+
+    showPrompt() {
         this.rl.prompt()
     }
 
-    showPrompt () {
-        this.rl.prompt()
-    }
-
-    updatePrompt () {
+    updatePrompt() {
         this.rl.setPrompt(`\n~> ${process.cwd()} > `)
     }
 
-    onCommand (callback) {
+    onCommand(callback) {
         this.rl.on('line', callback)
     }
 
